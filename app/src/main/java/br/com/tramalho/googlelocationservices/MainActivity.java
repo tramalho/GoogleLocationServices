f6075cbb7bdb67c05efa66295c4c7189911da688
tramalho <tramalho@ciandt.com>
2016-07-22 12:33:30 -0300

    add GoogleAPIClient

--- app/src/main/java/br/com/tramalho/googlelocationservices/MainActivity.java
+++ app/src/main/java/br/com/tramalho/googlelocationservices/MainActivity.java
@@ -1,13 +1,59 @@
 package br.com.tramalho.googlelocationservices;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+    private TextView txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createGoogleAPIClient();
+        loadUI();
+
+    }
+
+    private void loadUI() {
+        txtOutput = (TextView) findViewById(R.id.txt_output);
+    }
+
+    private void createGoogleAPIClient() {
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+
+        googleApiClient = builder
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }

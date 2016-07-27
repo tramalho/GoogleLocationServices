8c2e64f3f0fcddfe8fd821c4a39ac27112e0b743
tramalho <tramalho@ciandt.com>
2016-07-27 13:46:27 -0300

    onConnected

--- app/src/main/java/br/com/tramalho/googlelocationservices/MainActivity.java
+++ app/src/main/java/br/com/tramalho/googlelocationservices/MainActivity.java
@@ -1,23 +1,32 @@
 package br.com.tramalho.googlelocationservices;
 
 import android.location.Location;
-import android.location.LocationListener;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private final String TAG = this.getClass().getSimpleName();
+
 
     private GoogleApiClient googleApiClient;
     private TextView txtOutput;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +52,47 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
 
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        showLog("onConnected");
+
+            locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(1000);
+
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(googleApiClient, locationRequest, this);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        showLog("onConnectionSuspended", "status: "+i);
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        showLog("onConnectionFailed", connectionResult.getErrorMessage());
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        showLog("onLocationChanged", location.toString());
+        txtOutput.append("\n "+location.toString());
+    }
+
+    private void showLog(String message, String... addictional){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message +" - "+Arrays.toString(addictional));
     }
 }
